d3c3af8992e3ea931d294922f6f9c84baca970a4
Taezyn <62382364+Taezyn@users.noreply.github.com>
2020-03-19 12:34:13 +0100

    Add files via upload

---
+++ components/fighter.py
@@ -0,0 +1,39 @@
+import tcod as libtcod
+
+from game_messages import Message
+
+
+class Fighter:
+    def __init__(self, hp, defense, power, xp=0):
+        self.max_hp = hp
+        self.hp = hp
+        self.defense = defense
+        self.power = power
+        self.xp = xp
+
+    def take_damage(self, amount):
+        results = []
+        self.hp -= amount
+        if self.hp <= 0:
+            results.append({'dead': self.owner, 'xp': self.xp})
+        return results
+
+    def heal(self, *args):
+        if len(args) != 0:
+            self.hp += args[0]
+        else:
+            self.hp += self.max_hp//4
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
+    def attack(self, target):
+        results = []
+        damage = self.power - target.fighter.defense
+        if damage > 0:
+            results.append({'message': Message('{0} attaque {1} pour {2} PV.'.format(
+                self.owner.name.capitalize(), target.name, str(damage)), libtcod.white)})
+            results.extend(target.fighter.take_damage(damage))
+        else:
+            results.append({'message': Message('{0} attaque {1} mais ne fait pas de degats.'.format(
+                self.owner.name.capitalize(), target.name), libtcod.white)})
+        return results
