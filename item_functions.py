a9c6a188f22aa171330d02e4e183cfa90d537a50
Taezyn <62382364+Taezyn@users.noreply.github.com>
2020-03-19 12:25:53 +0100

    Add files via upload

---
+++ item_functions.py
@@ -0,0 +1,78 @@
+import tcod as libtcod
+from game_messages import *
+from components.ai import ConfusedMonster
+
+
+def heal(*args, **kwargs):
+    entity = args[0]
+    results = []
+    if entity.fighter.hp == entity.fighter.max_hp:
+        results.append({'consumed': False, 'message': Message('Deja full life', libtcod.yellow)})
+    else:
+        entity.fighter.heal()
+        results.append({'consumed': True, 'message': Message('25% PV rendus', libtcod.green)})
+    return results
+
+
+def cast_lightning(*args, **kwargs):
+    caster = args[0]
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+    results = []
+    target = None
+    closest_distance = maximum_range + 1
+    for entity in entities:
+        if entity.fighter and entity != caster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+            distance = caster.distance_to(entity)
+            if distance < closest_distance:
+                target = entity
+                closest_distance = distance
+    if target:
+        results.append({'consumed': True, 'target': target, 'message': Message(
+                    'Un éclair inflige {0} a {1} !'.format(damage, target.name))})
+        results.extend(target.fighter.take_damage(damage))
+    else:
+        results.append({'consumed': False, 'target': None, 'message': Message("Pas d'enemis a portee")})
+    return results
+
+
+def cast_fireball(*args, **kwargs):
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    radius = kwargs.get('radius')
+    target_x = kwargs.get('target_x')
+    target_y = kwargs.get('target_y')
+    results = []
+    if not libtcod.map_is_in_fov(fov_map, target_x, target_y):
+        results.append({'consumed': False, 'message': Message('Hors de portee', libtcod.yellow)})
+        return results
+    results.append({'consumed': True, 'messsage': Message('Tu allumes le feu sur un rayon de {0}'.format(radius), libtcod.orange)})
+    for entity in entities:
+        if entity.distance(target_x, target_y) <= radius and entity.fighter:
+            results.append({'message': Message('{0} subit {1} degats de feu'.format(entity.name, damage), libtcod.orange)})
+            results.extend(entity.fighter.take_damage(damage))
+    return results
+
+
+def cast_confuse(*args, **kwargs):
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    target_x = kwargs.get('target_x')
+    target_y = kwargs.get('target_y')
+    results = []
+    if not libtcod.map_is_in_fov(fov_map, target_x, target_y):
+        results.append({'consumed': False, 'message': Message('Hors de portee', libtcod.yellow)})
+        return results
+    for entity in entities:
+        if entity.x == target_x and entity.y == target_y and entity.ai:
+            confused_ai = ConfusedMonster(entity.ai, 10)
+            confused_ai.owner = entity
+            entity.ai = confused_ai
+            results.append({'consumed': True, 'message': Message('{0} devient debile'.format(entity.name), libtcod.light_green)})
+            break
+    else:
+        results.append({'consumed': False, 'message': Message('Pas de cible', libtcod.yellow)})
+    return results
---
+++ menus.py
@@ -0,0 +1,94 @@
+import tcod as libtcod
+
+
+def menu(con, header, options, width, screen_width, screen_height):
+    if len(options) > 26: raise ValueError('Plus de 26 items.')
+    # calculate total height for the header (after auto-wrap) and one line per option
+    header_height = libtcod.console_get_height_rect(con, 0, 0, width, screen_height, header)
+    height = len(options) + header_height
+    # create an off-screen console that represents the menu's window
+    window = libtcod.console_new(width, height)
+    # print the header, with auto-wrap
+    libtcod.console_set_default_foreground(window, libtcod.white)
+    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
+    # print all the options
+    y = header_height
+    letter_index = ord('a')
+    for option_text in options:
+        if option_text != '':
+            text = '(' + chr(letter_index) + ') ' + option_text
+            libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+            y += 1
+            letter_index += 1
+        else:
+            text = ''
+            libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+            y += 1
+    # blit the contents of "window" to the root console
+    x = int(screen_width / 2 - width / 2)
+    y = int(screen_height / 2 - height / 2)
+    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+
+
+def inventory_menu(con, header, inventory, inventory_width, screen_width, screen_height):
+    # show a menu with each item of the inventory as an option
+    if len(inventory.items) == 0:
+        options = ['Inventaire vide.']
+    else:
+        options = [item.name for item in inventory.items]
+    menu(con, header, options, inventory_width, screen_width, screen_height)
+
+
+def main_menu(con, background_image, screen_width, screen_height):
+    libtcod.image_blit_2x(background_image, 0, 0, 0)
+    libtcod.console_set_default_foreground(0, libtcod.light_yellow)
+    libtcod.console_print_ex(0, int(screen_width/2), 2, libtcod.BKGND_NONE,
+                             libtcod.CENTER, "Rogue doesn't like")
+    libtcod.console_print_ex(0, int(screen_width/2), 3, libtcod.BKGND_NONE,
+                             libtcod.CENTER, 'By Crevette')
+    menu(con, '', ['Nouvelle partie', 'Continuer la partie precedente', '(Rage)quit', '', 'Son on/off', 'Commandes'], 24, screen_width, screen_height)
+
+
+def level_up_menu(con, header, player, menu_width, screen_width, screen_height):
+    options = ['Sante (+20 PV, actuellement {0})'.format(player.fighter.max_hp),
+               'Force (+1 attaque, actuellement {0})'.format(player.fighter.power),
+               'Defense (+1 defense, actuellement {0})'.format(player.fighter.defense)]
+    menu(con, header, options, menu_width, screen_width, screen_height)
+
+
+def character_screen(player, character_screen_width, character_screen_height, screen_width, screen_height):
+    window = libtcod.console_new(character_screen_width, character_screen_height)
+    libtcod.console_set_default_foreground(window, libtcod.white)
+    libtcod.console_print_rect_ex(window, 0, 1, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, "Informations d'@")
+    libtcod.console_print_rect_ex(window, 0, 2, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'Niveau : {0}'.format(player.level.current_level))
+    libtcod.console_print_rect_ex(window, 0, 3, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'XP : {0}'.format(player.level.current_xp))
+    libtcod.console_print_rect_ex(window, 0, 4, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'XP avant le prochain niveau : {0}'.format(player.level.experience_to_next_level))
+    libtcod.console_print_rect_ex(window, 0, 6, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'PV max : {0}'.format(player.fighter.max_hp))
+    libtcod.console_print_rect_ex(window, 0, 7, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'Attaque: {0}'.format(player.fighter.power))
+    libtcod.console_print_rect_ex(window, 0, 8, character_screen_width, character_screen_height, libtcod.BKGND_NONE,
+                                  libtcod.LEFT, 'Defense: {0}'.format(player.fighter.defense))
+    x = screen_width // 2 - character_screen_width // 2
+    y = screen_height // 2 - character_screen_height // 2
+    libtcod.console_blit(window, 0, 0, character_screen_width, character_screen_height, 0, x, y, 1.0, 0.7)
+
+
+def message_box(con, header, width, screen_width, screen_height):
+    menu(con, header, [], width, screen_width, screen_height)
+
+
+def command_menu(con, background_image, screen_width, screen_height):
+    libtcod.image_blit_2x(background_image, 0, 0, 0)
+    libtcod.console_set_default_foreground(0, libtcod.light_yellow)
+    libtcod.console_print_ex(0, int(screen_width/2), 2, libtcod.BKGND_NONE,
+                             libtcod.CENTER, "Rogue doesn't like")
+    libtcod.console_print_ex(0, int(screen_width/2), 3, libtcod.BKGND_NONE,
+                             libtcod.CENTER, 'By Crevette')
+    menu(con, '', ['Deplacement avec les fleches', 'Diagonale : Y,U,B,N', 'Attraper un item : G',
+                   'Lacher un item : D', 'Prendre les escaliers > : Entree', 'Menu principal : Echap'],
+         40, screen_width, screen_height)
