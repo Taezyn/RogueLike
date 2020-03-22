42e94a687c3934b056538d121e0981d883558ed3
Taezyn <62382364+Taezyn@users.noreply.github.com>
2020-03-22 13:48:29 +0100

    Add files via upload

--- death_functions.py
+++ death_functions.py
@@ -2,18 +2,22 @@ import tcod as libtcod
 from game_messages import Message
 from game_states import GameStates
 from render_functions import RenderOrder
+import sound_manager.sound_manager as sm
+
+sound = sm.init_son()
+death_sounds = sound.get('death')[0]
 
 
 def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
-
+    death_sound = sm.Son(death_sounds)
+    death_sound.playpause()
     return Message('Rekt', libtcod.red), GameStates.PLAYER_DEAD
 
 
 def kill_monster(monster):
     death_message = Message('{0} est mort'.format(monster.name.capitalize()), libtcod.orange)
-
     monster.char = '%'
     monster.color = libtcod.dark_red
     monster.blocks = False
@@ -21,5 +25,4 @@ def kill_monster(monster):
     monster.ai = None
     monster.name = 'remains of ' + monster.name
     monster.render_order = RenderOrder.CORPSE
-
     return death_message
